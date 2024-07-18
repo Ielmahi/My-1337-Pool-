ce4d6b220b11dcb1486a82fe5799781b2801e56b
Ilias Elmahi <ilelmahi@e2r7p1.1337.ma>
2024-07-18 09:33:52 +0100

    C06✅

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilelmahi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/10 12:06:36 by ilelmahi          #+#    #+#             */
+/*   Updated: 2024/07/10 12:08:14 by ilelmahi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_sort_params(int ac, char *av[])
+{
+	int	sorted;
+	int	j;
+
+	sorted = 0;
+	while (!sorted)
+	{
+		sorted = 1;
+		j = 1;
+		while (j < ac - 1)
+		{
+			if (ft_strcmp(av[j], av[j + 1]) > 0)
+			{
+				ft_swap(&av[j], &av[j + 1]);
+				sorted = 0;
+			}
+			j++;
+		}
+	}
+}
+
+int	main(int ac, char *av[])
+{
+	int	i;
+
+	i = 1;
+	if (ac > 1)
+	{
+		ft_sort_params(ac, av);
+		while (i < ac)
+		{
+			ft_putstr(av[i]);
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+	return (0);
+}
